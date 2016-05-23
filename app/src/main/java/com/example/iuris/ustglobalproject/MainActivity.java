134ea5db631ab05da53c1d23e973fd81fbe13e8e
Lehmer <lehmer5@gmail.com>
2016-05-23 23:13:37 +0200

    Añadido código de llamada y mail (para todas las API) a partir de click. Falta que se distinga entre mail o llamada automáticamente (según sea teléfono o dirección de correo electrónico)

--- app/src/main/java/com/example/iuris/ustglobalproject/MainActivity.java
+++ app/src/main/java/com/example/iuris/ustglobalproject/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button buscar;
     private EditText textoBusqueda;
+    private static final int PHONE_CALL = 0;
+    Intent callIntent, emailIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +33,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    int identificador;
+    @Override
+    public void onClick(View v) {
+
+        int identificador;
+        identificador = v.getId();
+        TextView tv = (TextView) this.findViewById(identificador);
+        String datosDestino = (String) tv.getText();
+        call(tv);
+        //mail(tv);
+
+    }
+
+    private void mail(TextView tv){
+        emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        //emailIntent.setType("text/plain");
+        //emailIntent.putExtra(Intent.EXTRA_SUBJECT, "asunto"); //opcional
+        //emailIntent.putExtra(Intent.EXTRA_TEXT, "texto del correo"); //opcional
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {(String) tv.getText()}); //ok
+        startActivity(emailIntent);
+    }
+
+    private void call(TextView tv) {
+        callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + tv.getText()));
+        if (Build.VERSION.SDK_INT < 23) {
+            startActivity(callIntent); //hasta la API 22
+        } else {
+            if (verifyCallPermission()) {
+                startActivity(callIntent); //ya tiene permisos
+            } else {
+                requestPermission();
+            }
+        }
+    }
 
 
     public void buscar(View v){
@@ -44,52 +80,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-
-    @Override
-    public void onClick(View v) { //SI NECESITAIS TOCAR ALGO DE ESTA FUNCIÓN, AVISAD ANTES
-
-        identificador = v.getId();
-        TextView tv = (TextView) this.findViewById(identificador);
-        String datosDestino = (String) tv.getText();
-
-/*
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setType("message/rfc822");
-        //emailIntent.setType("text/plain");
-        //emailIntent.putExtra(Intent.EXTRA_SUBJECT, "asunto"); //opcional
-        //emailIntent.putExtra(Intent.EXTRA_TEXT, "texto del correo"); //opcional
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {(String) tv.getText()}); //ok
-        startActivity(emailIntent);
-*/
-
-        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:" + tv.getText()));
-        if (Build.VERSION.SDK_INT < 23) {
-            startActivity(callIntent); //hasta la API 22
-        } else {
-            if (verifyPermission()) {
-                startActivity(callIntent); //ya tiene permisos
-            } else { //en desarrollo
-                //requestPermission();
-            }
-        }
-
-    }
-
     //Paso 1. Verificar permiso
-    private boolean verifyPermission() {
+    private boolean verifyCallPermission() {
 
         //CALL_PHONE tiene implícitos READ_PHONE_STATE, READ_CALL_LOG, WRITE_CALL_LOG,
         // ADD_VOICEMAIL, USE_SIP y PROCESS_OUTGOING_CALLS porque pertenecen al mismo
         //grupo de permisos
 
-        int callPermission = checkSelfPermission(Manifest.permission.CALL_PHONE);
-        //ni caso al error -> no es problema pues solo se llama a partir de la API 23
+        int callPermission = checkSelfPermission(Manifest.permission.CALL_PHONE); //no es problema pues solo se llama a partir de la API 23
 
-        if (callPermission != PackageManager.PERMISSION_GRANTED) {
+        if (callPermission != PackageManager.PERMISSION_GRANTED) { //no hay permiso previamente
             return false;
-        } else {
+        } else { //ya dimos permiso con anterioridad
             return true;
         }
+    }
+
+    //Paso 2: Solicitar permiso
+    private void requestPermission() { //hasta aquí llega bien
+        //shouldShowRequestPermissionRationale es verdadero solamente si ya se había mostrado
+        //anteriormente el dialogo de permisos y el usuario lo negó
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CALL_PHONE)) {
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CALL); //no es problema pues solo se llama a partir de la API 23
+            Log.i("Caso: ","Permisos previamente negados");
+        } else {
+            //y falso si es la primera vez se solicita el permiso directamente
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CALL); //no es problema pues solo se llama a partir de la API 23
+            Log.i("Caso: ","Petición de permisos por primera vez");
+        }
+    }
+
+    //Paso 3: Procesar respuesta de usuario
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //Si el requestCode corresponde al que usamos para solicitar el permiso y
+        //la respuesta del usuario fue positiva
+        //if (requestCode == PHONE_CALL) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.i("Respuesta: ","Permiso concedido");
+            startActivity(callIntent);
+        } else {
+            Log.i("Respuesta: ","Permiso denegado");
+//                showSnackBar();
+        }
+        //}
     }
 }
