40607245168bd5b208d7679e2d6d7f6eb97b5e75
Andrea Agudo <Andrea Agudo>
2016-05-24 09:27:09 +0200

    Login creado con la interfaz

---
+++ app/src/main/java/com/example/iuris/ustglobalproject/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.iuris.ustglobalproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by andreagudo3 on 24/05/2016.
+ */
+public class LoginActivity extends Activity {
+    Button blogin,bcancelar;
+    EditText ed1,ed2;
+
+    TextView txtintentos;
+    int contador = 3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        blogin=(Button)findViewById(R.id.button);
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+
+        bcancelar=(Button)findViewById(R.id.button2);
+        blogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(ed1.getText().toString().equals("admin") && ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Cargando...",Toast.LENGTH_SHORT).show();
+                    Intent ventanaSearch = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(ventanaSearch);
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrectos",Toast.LENGTH_SHORT).show();
+
+                    contador--;
+
+                    if (contador == 0) {
+                        Toast.makeText(getApplicationContext(), "Demasiados intentos, login deshabilitado",Toast.LENGTH_SHORT).show();
+                        blogin.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        bcancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
