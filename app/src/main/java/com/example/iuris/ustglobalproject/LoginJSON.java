d34cd442b0771260a3e2afae8ef7c60be3f1c4d4
Andrea Agudo <Andrea Agudo>
2016-05-26 09:30:29 +0200

    Retrofit build.gradle

--- app/src/main/java/com/example/iuris/ustglobalproject/LoginJSON.java
+++ app/src/main/java/com/example/iuris/ustglobalproject/LoginJSON.java
@@ -1,40 +1,42 @@
 package com.example.iuris.ustglobalproject;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 /**
  * Created by andreagudo3 on 25/05/2016.
  */
 public class LoginJSON {
-    private String login;
-    private String password;
 
-    public String getLogin() {
-        return login;
-    }
+        @SerializedName("login")
+        @Expose
+        private String login;
+        @SerializedName("password")
+        @Expose
+        private String password;
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String toJSON() {
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("login", getLogin());
-            jsonObject.put("password", getPassword());
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
+        /**
+         * @return The login
+         */
+        public String getLogin() {
+            return login;
         }
-        return null;
-    }
+
+        /**
+         * @param login The login
+         */
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        /**
+         * @return The password
+         */
+        public String getPassword() {
+            return password;
+        }
+
+        /**
+         * @param password The password
+         */
+        public void setPassword(String password) {
+            this.password = password;
+        }
 }
