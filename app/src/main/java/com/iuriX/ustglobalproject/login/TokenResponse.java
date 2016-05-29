10cd6147b0b4b8a14818379611202096917aeba3
Andrea Agudo <Andrea Agudo>
2016-05-29 18:07:05 +0200

    LOGIN RETROFIT REST POST. INTENTO

--- app/src/main/java/com/iuriX/ustglobalproject/login/TokenResponse.java
+++ app/src/main/java/com/iuriX/ustglobalproject/login/TokenResponse.java
@@ -1,4 +1,4 @@
-package com.example.iuris.ustglobalproject.login;
+package com.iuriX.ustglobalproject.login;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
