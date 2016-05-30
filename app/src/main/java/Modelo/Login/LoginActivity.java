5e9397957421e50279dbc9d41a7f11e4cd547ecb
Andrea Agudo <Andrea Agudo>
2016-05-30 09:21:17 +0200

    Corrección R LoginActivity

--- app/src/main/java/Modelo/Login/LoginActivity.java
+++ app/src/main/java/Modelo/Login/LoginActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.iuriX.ustglobalproject.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
