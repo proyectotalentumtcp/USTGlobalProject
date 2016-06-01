b9124bc26c7793fcc57f99b397d9841e9e1b441d
Lehmer <lehmer5@gmail.com>
2016-06-01 22:27:07 +0200

    Permisos (por revisar) de llamadas

--- app/src/main/java/controlador/AdaptadorUsuario.java
+++ app/src/main/java/controlador/AdaptadorUsuario.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
@@ -24,6 +25,8 @@ import modelo.Session;
 import modelo.busqueda.ListaEmpleados;
 import modelo.login.R;
 
+import static android.support.v4.app.ActivityCompat.requestPermissions;
+
 /**
  * Created by Miguel Rodríguez on 23/05/2016.
  */
@@ -39,6 +42,7 @@ public class AdaptadorUsuario extends RecyclerView.Adapter<AdaptadorUsuario.Usua
         public TextView nombre;
         public TextView apellidos;
         public ImageView imagen;
+        private static final int PHONE_CALL = 0;
 
         ImageView botonLlamar, botonMail;
 
@@ -57,26 +61,22 @@ public class AdaptadorUsuario extends RecyclerView.Adapter<AdaptadorUsuario.Usua
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(v.getContext(), "Llamada para: " + movil, Toast.LENGTH_SHORT).show();
-
+                    Context context = v.getContext();
 
                     Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(movil));
-                    Context context = v.getContext();
-                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
-                        return;
+                    if (Build.VERSION.SDK_INT < 23) {
+                        context.startActivity(intent); //hasta la API 22
+                    } else {
+                        if (verifyCallPermission()) {
+                            context.startActivity(intent); //ya tiene permisos
+                        } else {
+                            requestPermission();
+                        }
                     }
-                    context.startActivity(intent);
-
-
 
                 }
             });
+
 
             botonMail.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -94,6 +94,55 @@ public class AdaptadorUsuario extends RecyclerView.Adapter<AdaptadorUsuario.Usua
                 }
             });
 
+        }
+
+        //Paso 1. Verificar permisos
+        private boolean verifyCallPermission() {
+
+            // CALL_PHONE tiene implícitos READ_PHONE_STATE, READ_CALL_LOG, WRITE_CALL_LOG,
+            // ADD_VOICEMAIL, USE_SIP y PROCESS_OUTGOING_CALLS porque pertenecen al mismo
+            // grupo de permisos
+
+            int callPermission = checkSelfPermission(Manifest.permission.CALL_PHONE); //no es problema pues solo se llama a partir de la API 23
+
+            if (callPermission != PackageManager.PERMISSION_GRANTED) { //no hay permiso previamente
+                return false;
+            } else { //ya dimos permiso con anterioridad
+                return true;
+            }
+        }
+
+        //Paso 2: Solicitar permiso
+        private void requestPermission() { //hasta aquí llega bien
+            //shouldShowRequestPermissionRationale es verdadero solamente si ya se había mostrado
+            //anteriormente el dialogo de permisos y el usuario lo negó
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CALL_PHONE)) {
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CALL); //no es problema pues solo se llama a partir de la API 23
+                Log.i("Caso: ","Permisos previamente negados");
+            } else {
+                //y falso si es la primera vez se solicita el permiso directamente
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CALL); //no es problema pues solo se llama a partir de la API 23
+                Log.i("Caso: ","Petición de permisos por primera vez");
+            }
+        }
+
+        //Paso 3: Procesar respuesta de usuario
+        @Override
+        public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            //Si el requestCode corresponde al que usamos para solicitar el permiso y
+            //la respuesta del usuario fue positiva
+            //if (requestCode == PHONE_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("Respuesta: ","Permiso concedido");
+                context.startActivity(intent);
+            } else {
+                Log.i("Respuesta: ","Permiso denegado");
+//                showSnackBar();
+            }
+            //}
         }
     }
 
