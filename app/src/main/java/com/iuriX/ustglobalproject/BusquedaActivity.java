336c58f9b127026d4fdfd8b0ab8c00848f45a9d1
mrr93 <miguel.rodriguezruiz@hotmail.com>
2016-06-01 10:14:47 +0200

    Cambio de nombre de la aplicacion al instalarla en un terminal

--- app/src/main/java/com/iuriX/ustglobalproject/BusquedaActivity.java
+++ app/src/main/java/com/iuriX/ustglobalproject/BusquedaActivity.java
@@ -3,17 +3,30 @@ package com.iuriX.ustglobalproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import controlador.AdaptadorUsuario;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import modelo.Session;
+import modelo.busqueda.BusquedaInterface;
 import modelo.busqueda.ListaEmpleados;
+import modelo.busqueda.PeticionBusquedaJSON;
 import modelo.login.R;
 import modelo.Usuario;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class BusquedaActivity extends Activity {
@@ -21,6 +34,8 @@ public class BusquedaActivity extends Activity {
     private RecyclerView listaUsuarios;
     private RecyclerView.Adapter adaptador;
     private RecyclerView.LayoutManager lManager;
+    private TextView buscarBusqueda;
+    private BusquedaInterface service;
 
 
     @Override
@@ -39,6 +54,112 @@ public class BusquedaActivity extends Activity {
         adaptador = new AdaptadorUsuario(Session.getInstance().getListaEmpleadosSession());
         listaUsuarios.setAdapter(adaptador);
 
+    }
+    public void buscar(View v){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.api_url))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        service = retrofit.create(BusquedaInterface.class);
+
+        //buscarBusqueda.setError(null);
+
+        String busqueda = buscarBusqueda.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (busqueda.equals("")){
+
+            buscarBusqueda.setError("Campo de busqueda vacío");
+            focusView = buscarBusqueda;
+            //cancel = true; //cancelado para aligerar testea
+
+        }
+
+        if (cancel){
+            focusView.requestFocus();
+        }else{
+            final PeticionBusquedaJSON peticionBusquedaJSON = new PeticionBusquedaJSON();
+            final ListaEmpleados listaEmpleados = new ListaEmpleados();
+
+            peticionBusquedaJSON.setBusqueda(buscarBusqueda.getText().toString());
+            peticionBusquedaJSON.setSessionId(Session.getInstance().getSessionId());
+
+
+            Call<ListaEmpleados> listaEmpleadosCall = service.getListaEmpleados(peticionBusquedaJSON);
+            listaEmpleadosCall.enqueue(new Callback<ListaEmpleados>() {
+                @Override
+                public void onResponse(Call<ListaEmpleados> call, Response<ListaEmpleados> response) {
+
+                    int statusCode = response.code();
+
+                    ListaEmpleados listaEmpleados1 = response.body();
+
+                    //Session.getInstance().setId_empleado_seleccionado(listaEmpleados1.empleados.get().getId());
+
+                    if (listaEmpleados1.getListaUsuarios().size() > 0) {
+
+                        Log.d("MainActivity", "onResponse" + statusCode + " " + listaEmpleados1);
+
+
+                        Session.getInstance().setListaEmpleadosSession(listaEmpleados1);
+                        adaptador.notifyDataSetChanged();
+
+
+                    } else {
+
+                        Toast.makeText(BusquedaActivity.this, "Lista de empleados vacía", Toast.LENGTH_SHORT).show();
+
+                    }
+
+
+
+
+                }
+
+                @Override
+                public void onFailure(Call<ListaEmpleados> call, Throwable t) {
+
+                    Log.d("LoginActivity", "onFailure: " + t.getMessage());
+
+                }
+
+            });
+
+
+        }
+
+    }
+
+    boolean twice;
+    @Override
+    public void onBackPressed() {
+
+        Log.d("BusquedaActivity","click");
+
+        if (twice == true) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+            System.exit(0);
+
+        }
+        twice = true;
+        Log.d("MainActivity", "twice: " + twice);
+
+        Toast.makeText(BusquedaActivity.this,"Presiona ATRÁS de nuevo para salir", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                twice = false;
+                Log.d("MainActivity", "twice: " + twice);
+            }
+        },3000);
     }
 
 
